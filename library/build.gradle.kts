e33ff6a5ac60a4d59628df2c36d891fea59197e5
Amsavarthan <80940598+amsavarthan@users.noreply.github.com>
2024-04-17 14:45:49 +0530

    Refactored label as a tooltip for reaction

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.foundation)
+    implementation(libs.androidx.material3)
 
 }
 
